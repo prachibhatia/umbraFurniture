8fcfff788204ad905c577508d0eda55d16678183
prachibhatia <prachi24bhatia@gmail.com>
2019-07-22 16:36:11 +0530

    second commit

--- client/script.js
+++ client/script.js
@@ -7,6 +7,7 @@ window.onload = function(event) {
     contact_no.value="";
     email_id.value="";
     query.value="";
+
   }
 querySubmit.addEventListener('click',function(event){
     event.preventDefault()
@@ -40,12 +41,12 @@ let image3 = document.getElementById('box3');
 let image4 = document.getElementById('box4');
 let image5 = document.getElementById('box5');
 let image6 = document.getElementById('box6');
-console.log(image1.childNodes[3].src);
-console.log(image2.childNodes[3].src);
+// console.log(image1.childNodes[3].src);
+// console.log(image2.childNodes[3].src);
 fetch('http://localhost:1804/',
       ).then(res => res.json())
         .then(status =>{
-          console.log(status[0].image_url);
+        //   console.log(status[0].image_url);
           image1.childNodes[3].src = status[0].image_url;
           image2.childNodes[3].src = status[1].image_url;
           image3.childNodes[3].src = status[2].image_url;
@@ -53,4 +54,32 @@ fetch('http://localhost:1804/',
           image5.childNodes[3].src = status[4].image_url;
           image6.childNodes[3].src = status[5].image_url;
         })
-
+let discounturl = document.getElementById('discountImage');
+// console.log(discounturl.src);
+let discountPerc = document.getElementById('discountPercentage');
+// console.log(discountPerc.innerHTML);
+fetch('http://localhost:1804/deals',
+      ).then(res => res.json())
+        .then(resp =>{
+          //console.log(resp);
+          discounturl.src = resp[0].discountURL;
+          discountPerc.innerHTML = resp[0].discountPerc;
+        })
+let header1 = document.getElementById("head1");
+let header2 = document.getElementById("head2");
+let headerPara1 = document.getElementById("head_para1");
+let headerPara2 = document.getElementById("head_para2");
+console.log(header1.innerText);
+console.log(header2.innerText);
+fetch('http://localhost:1804/latest',
+      ).then(res => res.json())
+        .then(resp =>{
+            header1.innerText = resp[0].header;
+            header2.innerText = resp[1].header;
+            headerPara1.innerText = resp[0].header_info;
+            headerPara2.innerText = resp[1].header_info;
+        })
+// function btnsub(event){
+//     this.event.preventDefault();
+//     console.log("hello");
+// }
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.port || 1804
 const db = require('./db/connection')
 const swal = require('sweetalert')
+//import swal from 'sweetalert';
 
 app.use(express.json())
 app.use(express.urlencoded(
@@ -11,6 +12,7 @@ app.use(express.urlencoded(
           extended : true
      }
 ))
+
 app.post('/queries',(req,res)=>{
     console.log(req.body);
     db.getConnection((err , connection) =>{
@@ -38,8 +40,16 @@ app.post('/queries',(req,res)=>{
 app.use('/home' , express.static('../client'))
 
 app.get('/admin',(req,res) => {
-    //   res.setHeader('Content-Type' , "text/html")
+       res.setHeader('Content-Type' , "text/html")
       res.sendFile(path.join(__dirname,'../client/admin.html'))
+})
+app.get('/discount',(req,res) => {
+       res.setHeader('Content-Type' , "text/html")
+      res.sendFile(path.join(__dirname,'../client/discount.html'))
+})
+app.get('/latestNews',(req,res) => {
+    res.setHeader('Content-Type' , "text/html")
+   res.sendFile(path.join(__dirname,'../client/latestNews.html'))
 })
 
 app.get('/',(req,res)=>{
@@ -64,6 +74,116 @@ app.get('/',(req,res)=>{
         }
    })
 })
+app.get('/latest',(req,res)=>{
+    db.getConnection((err , connection) =>{
+        if(err){
+             console.log(err);
+             connection.release();
+             return ; 
+        }
+        else{
+             //console.log("Hello")
+             connection.query(`SELECT header,header_info FROM latest_news` , (err ,rows , fields) =>{
+                  if(err){
+                       console.log(err)
+                       return res.send(err)
+                  }
+                  else {
+                  res.send(rows);
+                  connection.release()
+                  }
+             })
+        }
+   })
+})
+app.get('/deals',(req,res)=>{
+    console.log("hello")
+    db.getConnection((err , connection) =>{
+        if(err){
+             console.log(err);
+             connection.release();
+             return ; 
+        }
+        else{
+             console.log("Hello")
+             connection.query(`SELECT discountPerc,discountURL FROM myDiscount` , (err ,rows , fields) =>{
+                  if(err){
+                       console.log(err)
+                       return res.send(err)
+                  }
+                  else {
+                  res.send(rows);
+                  connection.release()
+                  }
+             })
+        }
+   })
+})
+app.post('/discount',(req,res)=>{
+    //console.log(req.body);
+    db.getConnection((err , connection) =>{
+        if(err){
+             console.log(err);
+             connection.release();
+             return ; 
+        }
+        else{
+             console.log("Hello")
+             connection.query(`UPDATE myDiscount SET discountURL='${req.body.urlDiscount}',discountPerc='${req.body.percDiscount}'` , (err ,rows , fields) =>{
+                  if(err){
+                       console.log(err)
+                       return res.send(err)
+                  }
+             })
+    }
+   })
+})
+app.post('/latest1',(req,res)=>{
+    //console.log(req.body);
+    db.getConnection((err , connection) =>{
+        if(err){
+             console.log(err);
+             connection.release();
+             return ; 
+        }
+        else{
+             //console.log("Hello")
+             connection.query(`UPDATE latest_news SET header='${req.body.header1}',header_info='${req.body.headerPara1}' WHERE id='1'` , (err ,rows , fields) =>{
+                  if(err){
+                       console.log(err)
+                       return res.send(err)
+                  }
+                  else {
+                        console.log("hello")
+                        res.send('<script>alert("Webpage updated !")</script>')
+                    connection.release()
+                    }
+             })
+    }
+   })
+})
+app.post('/latest2',(req,res)=>{
+    //console.log(req.body);
+    db.getConnection((err , connection) =>{
+        if(err){
+             console.log(err);
+             connection.release();
+             return ; 
+        }
+        else{
+             //console.log("Hello")
+             connection.query(`UPDATE latest_news SET header='${req.body.header2}',header_info='${req.body.headerPara2}' WHERE id='2'` , (err ,rows , fields) =>{
+                  if(err){
+                       console.log(err)
+                       return res.send(err)
+                  }
+                  else {
+                    connection.release()
+                    }
+             })
+    }
+   })
+})
 app.post('/newimages',(req,res)=>{
     console.log(req.body);
     db.getConnection((err , connection) =>{
@@ -82,10 +202,9 @@ app.post('/newimages',(req,res)=>{
                        console.log(err)
                        return res.send(err)
                   }
-                //   else {
-                //   //res.send(rows);
-                //   //connection.release()
-                //   }
+                  else {
+                    connection.release()
+                    }
              })
         }
     }
